30dcbd80145704c03a60cffd41a883f4325bd107
Raunakkumarmaurya <raunakssm802215@gmail.com>
2024-06-26 15:27:12 +0530

    Add files via upload

---
+++ Animal4.java
@@ -0,0 +1,19 @@
+package DataType;
+
+public class Animal4 {
+
+  public String raunak()
+  {
+      System.out.println("papa,papa,papa,papa");
+      return ("papa,papa,papa,papa");
+  }
+
+  public String rashamika()
+  {
+      System.out.println("----------------------------");
+      return("--------------------------");
+  }
+
+
+
+}
